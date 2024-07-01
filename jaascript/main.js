ea9d1b70ddee45d8f73613ea3ee873157e4afc7d
carp1nch0 <168030659+carp1nch0@users.noreply.github.com>
2024-07-01 19:15:30 -0300

    Update main.js

--- jaascript/main.js
+++ jaascript/main.js
@@ -1,26 +1,26 @@
-alert("violacion de leyes");
-alert("el FBI quiere saber tu ubicacion");
+alert("a ingresado a venta retro, mantener discrecion")
 
-let age = prompt ('¿les das tu ubicacion?', "haga click en aceptar para darselas o cancelar para no darselas");
+function animateText(textArea) {
+    let text = textArea.value;
+    let to = text.length,
+      from = 0;
 
-alert(`entendido`); 
+    animate({
+      duration: 5000,
+      timing: bounce,
+      draw: function(progress) {
+        let result = (to - from) * progress + from;
+        textArea.value = text.slice(0, Math.ceil(result))
+      }
+    });
+  }
 
-const text = "en sus aniversarios habra ofertas, el descuento de:";
-console.log(text);  /*meti el texto que esta en las comillas, lo meti dentro de la variable "text" y com "console.log" hise que me mostrara el texto en la consola de herramientas para desarrolladors*/
 
-const number = 9;  /*en este es lo mismo con el texto, pero con numeros*/ 
-console.log(number);
-
-const date = new Date("2024/06/06 18:40:39");
-console.log(date);   /*con el codigo "new date" meti la fecha y hora del dia que hice esto y eso lo meti en la variable "date", y use console.log para que lo mostrara*/ 
-
-const span = document.body.querySelector("span");
-document.body.addEventListener("click", (event) => {
-    const { x, y, detail } = event;
-    span.textContent = `Has hecho ${detail} CLICK en las coordenadas (${x}x${y})`;
-});
-console.log(span);
-console.log({ x, y, detail });  /*aqui solo copie y pegue el codigo, no solo en java sino tambien otros codigos al css y el html, con unos cambios para que se vieran en la pagina; lo que hace es mostrarte las coordenadas en donde hiciste click*/
-console.log(span.textContent);
-
+  function bounce(timeFraction) {
+    for (let a = 0, b = 1; 1; a += b, b /= 2) {
+      if (timeFraction >= (7 - 4 * a) / 11) {
+        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+      }
+    }
+  }
 
